5b88af419d10f29e3273679d67da07db838ad93d
Martin-Strelec <157502550+Martin-Strelec@users.noreply.github.com>
2024-05-18 09:58:37 +0100

    Added explainatory comments

--- codeSnippets/CSharp/StrModifier.cs
+++ codeSnippets/CSharp/StrModifier.cs
@@ -11,12 +11,14 @@ namespace ExamMock2
         {
         }
 
-        public string SimplifyString(string input) //only input lowercase characters
+        public string SimplifyString(string input) //only input lowercase characters. Outputs lowercase string with removed whitespaces
         {
+            //Variables
             List<int> removeIndex = new List<int>();
             string inputTemp = input.ToLower();
             char rmChar = ' ';
-
+            
+            //Loop seraches for match with the rmChar variable
             for (int i = 0; i < input.Length; i++)
             {
                 if (inputTemp[i] == rmChar)
@@ -25,6 +27,7 @@ namespace ExamMock2
                 }
             }
 
+            //Loop removes indexes defined by the previous loop
             for (int i = 0; i < removeIndex.Count; i++)
             {
                 inputTemp = inputTemp.Remove(removeIndex[i] - i, 1);
@@ -32,11 +35,13 @@ namespace ExamMock2
             return inputTemp;
         }
 
-        public string RemoveChar(char rmChar, string input) //only input lowercase characters
+        public string RemoveChar(char rmChar, string input) //only input lowercase characters. Outputs lowercase string with removed characters
         {
+            //Variables
             List<int> removeIndex = new List<int>();
             string inputTemp = input.ToLower();
 
+            //Searches for match with the rmChar variable and stores their index
             for (int i = 0; i < input.Length; i++)
             {
                 if (inputTemp[i] == rmChar)
@@ -45,6 +50,7 @@ namespace ExamMock2
                 }
             }
 
+            //Removes indexes defined by the previous loop
             for (int i = 0; i < removeIndex.Count; i++)
             {
                 inputTemp = inputTemp.Remove(removeIndex[i] - i, 1);
